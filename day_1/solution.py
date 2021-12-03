8588440bb6937defa4ef7b5ad52d9ee081046bef
Alyssa Mallory <amallory@greatschools.org>
2021-12-02 18:37:50 -0800

    fixed some notes/naming due to confusion on two part question vs. day 1 and 2

---
+++ day_1/solution.py
@@ -0,0 +1,16 @@
+file = "input.txt"
+
+data = []
+
+for x in open(file, "r"):
+	data.append(int(x))
+
+solutions = {}
+
+# part 1
+solutions[1] = sum([data[x] > data[x-1] for x in range(1, len(data))])
+# part 2
+solutions[2] = sum([data[x] > data[x-3] for x in range(3, len(data))])
+
+for key,value in daily_solutions.items():
+	print(f"Question {key}: {value}")
