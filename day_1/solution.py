b9c6218134d4856c25a8a310739df9da30d23396
Alyssa Mallory <amallory@greatschools.org>
2021-12-02 18:38:20 -0800

    Bug-Aligning variable names

--- day_1/solution.py
+++ day_1/solution.py
@@ -12,5 +12,5 @@ solutions[1] = sum([data[x] > data[x-1] for x in range(1, len(data))])
 # part 2
 solutions[2] = sum([data[x] > data[x-3] for x in range(3, len(data))])
 
-for key,value in daily_solutions.items():
+for key,value in solutions.items():
 	print(f"Question {key}: {value}")
